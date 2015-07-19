35691cb27cb9384dc3c872ac925db484e61e32c8
kaosad <frasker@hotmail.com>
2015-07-19 17:01:10 +0800

    Add test for dtoa()

--- test/test_dtoa.c
+++ test/test_dtoa.c
@@ -11,3 +11,11 @@ void test_strtod_given_extremely_small_fraction_value() {
 	d = _strtod("12.3456789012e-210 abc", NULL);
   printf("%.12e\n", d);
 }
+
+void test_dtoa_given_fractional_value() {
+  char *value;
+  int sign, decpt;
+  value = _dtoa(-12.3456789012e+2, 10, 15, &decpt, &sign, NULL);
+  // Note sign=1 means negative number
+  printf("%s, decpt=%d, sign=%d\n", value, decpt, sign);
+}
